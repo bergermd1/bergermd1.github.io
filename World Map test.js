b370341ed4b3db99e2e5705fb736485c8c4c38b8
bergermd1 <88791701+bergermd1@users.noreply.github.com>
2022-04-15 12:12:29 -0500

    Add files via upload

---
+++ World Map test.js
@@ -0,0 +1,28 @@
+// import svgMap from 'svgmap';
+// import 'svgmap/dist/svgMap.min.css';
+
+new svgMap({
+    targetElementID: 'svgMap',
+    data: {
+      data: {
+        gdp: {
+          name: 'GDP per capita',
+          format: '{0} USD',
+          thousandSeparator: ',',
+          thresholdMax: 50000,
+          thresholdMin: 1000
+        },
+        change: {
+          name: 'Change to year before',
+          format: '{0} %'
+        }
+      },
+      applyData: 'gdp',
+      values: {
+        AF: { gdp: 587, change: 4.73 },
+        AL: { gdp: 4583, change: 11.09 },
+        DZ: { gdp: 4293, change: 10.01 }
+        // ...
+      }
+    }
+  });
